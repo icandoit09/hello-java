7697f1f66345f0fe10572978930c0a5221990a5c
app-15 <icandoit0928@hanmail.net>
2013-09-14 02:06:20 +0900

    initial commit

---
+++ src/ac/jeju/hello/HelloJava.java
@@ -0,0 +1,14 @@
+package ac.jeju.hello;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: app-15
+ * Date: 13. 9. 14.
+ * Time: 오전 1:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloJava {
+    public static void main(String[] args){
+        System.out.println("Hello Java");
+    }
+}
